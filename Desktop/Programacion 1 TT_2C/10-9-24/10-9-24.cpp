37441a600c1b9094a718e0440b5ff621169ee5a3
solivera <solivera@frsn.utn.edu.ar>
2024-09-10 15:39:42 -0300

    Modificaciones en IF

--- Desktop/Programacion 1 TT_2C/10-9-24/10-9-24.cpp
+++ Desktop/Programacion 1 TT_2C/10-9-24/10-9-24.cpp
@@ -12,12 +12,20 @@ int numero;
 cout<<"ingrese un numero: ";
 cin>> numero;
 
-for(int i=1;i <= numero; i++){
+if(numero <= 0){
+
+    cout<<"Ingrese un numero mayor a 0."<<endl;
+}else{
+    for(int i=1;i <= numero; i++){
 
     cout << numero << " x " << i << " = "<< numero * i<<endl ;
 
-}
 
-
+        }
+    }+
 return 0;
+
 }
+
+
+

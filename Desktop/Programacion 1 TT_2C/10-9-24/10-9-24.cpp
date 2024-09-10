93f254b58a3d9a8b91635cd8d6bed7d584dabae0
solivera <solivera@frsn.utn.edu.ar>
2024-09-10 15:14:06 -0300

    primer commit, crea tabla de multiplicar

---
+++ Desktop/Programacion 1 TT_2C/10-9-24/10-9-24.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main(){
+
+int numero;
+
+cout<<"ingrese un numero"<<endl;
+cin>> numero;
+
+for(int i=1;i <= numero; i++){
+
+    cout << numero << " x " << i << " = "<< numero * i<<endl ;
+
+}
+
+
+return 0;
+}
